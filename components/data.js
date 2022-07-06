5cae7834d4b57ea7015eadad0ba4e4dbca3e39f9
Dubjay18 <jejeniyi7@gmail.com>
2022-07-06 19:44:20 +0100

    feat: added more projects

--- components/data.js
+++ components/data.js
@@ -3,6 +3,9 @@ import chatpic from "../images/jaychats.jpg";
 import kodepic from "../images/kodeschool.jpg";
 import slackpic from "../images/slackclone.png";
 import pexelspic from "../images/jaypexel.jpg";
+import G34pic from "../images/G34-resturant.jpg";
+import Todo from "../images/JayTodo.jpg";
+import Gym from "../images/JayGym.jpg";
 const projectData = [
   {
     name: "JAYpexel",
@@ -34,6 +37,23 @@ const projectData = [
       "Firebase",
       "TMDB Api",
       "daisy UI",
+    ],
+  },
+  {
+    name: "JayTodo",
+    desp: "A Website that displays alot of exercises. The site has search functionality which helps uses search through the large number of exercises, it also has links to youtube videos for the exercises selected to give the user better understanding.",
+    image: Gym,
+    liveLink: "https://jay-gym.vercel.app/",
+    githubLink: "https://github.com/Dubjay18/JayGym",
+    tech: "Next Js",
+    topTech: "Next js/ Framer motion/ Tailwind CSS",
+    technologies: [
+      "Next js",
+      "ExerciseDB Api",
+      "React js",
+      "Framer motion",
+      "Tailwind CSS",
+      "Youtube Search Api",
     ],
   },
   {
@@ -73,6 +93,32 @@ const projectData = [
     tech: "React Js",
     topTech: "React js/ Material UI/ Firebase",
     technologies: ["React js", "Material UI", "Firebase"],
+  },
+  {
+    name: "G34-Resturant",
+    desp: "This is a basic website for a resturant which allows its customers to order food with few clicks.",
+    image: G34pic,
+    liveLink: "https://dubjay18.github.io/kodecampsolu/",
+    githubLink: "https://github.com/Dubjay18/kodecampsolu",
+    tech: "React Js",
+    topTech: "React js/ react-bootstrap ",
+    technologies: ["React js", "react-bootstrap", "axios", "bootstrap"],
+  },
+  {
+    name: "JayTodo",
+    desp: "A to-do list web app.",
+    image: Todo,
+    liveLink: "https://todofrontend-one.vercel.app/",
+    githubLink: "https://github.com/Dubjay18/Todofrontend",
+    tech: "React Js",
+    topTech: "React js/ MongoDb/ Node js",
+    technologies: [
+      "React js",
+      "Material UI",
+      "MongoDb",
+      "Node js",
+      "Express js",
+    ],
   },
 ];
 
